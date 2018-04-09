e63d574815dc6ddcdd19b3a425bb7fc8db6d8443
freyo <freyhsiao@gmail.com>
2018-04-09 21:37:37 +0800

    fix: listContents recursive

--- src/Adapter.php
+++ src/Adapter.php
@@ -335,7 +335,7 @@ class Adapter extends AbstractAdapter
 
         $response = $this->client->listObjects([
             'Bucket'    => $this->getBucket(),
-            'Prefix'    => empty($directory) ? $directory : rtrim($directory, '/') . '/',
+            'Prefix'    => ((string)$directory === '') ? '' : (rtrim($directory, '/') . '/'),
             'Delimiter' => $recursive ? '' : '/',
         ]);
 
