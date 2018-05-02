302c004364772c648c0a6834812775ff332c14c5
freyo <freyhsiao@gmail.com>
2018-05-02 13:28:50 +0800

    unknown region support

--- src/Adapter.php
+++ src/Adapter.php
@@ -78,7 +78,8 @@ class Adapter extends AbstractAdapter
      */
     public function getRegion()
     {
-        return $this->regionMap[$this->config['region']];
+        return array_key_exists($this->regionMap, $this->config['region']) 
+            ? $this->regionMap[$this->config['region']] : $this->config['region'];
     }
 
     /**
